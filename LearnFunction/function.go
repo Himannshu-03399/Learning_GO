390f9fd687dc35ab057c8105a2688c675e66c1f9
Himannshu-03399 <krhimanshuraj1@gmail.com>
2024-12-29 01:39:50 +0530

    Learning function

---
+++ LearnFunction/function.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to Learning functions in GO")
+	add := sum(10,5) // storing output of sum function into add variable
+	fmt.Println("Addition of two digits using sum is :" ,add)
+	product := multiply(5.9 , 8)
+	fmt.Println("Multiplication of x and y is" , product)
+
+}
+// sum(a int , b) this means we give datatypes to only a 
+func sum(a,b int ) (result int){ // we can also write sun( a int , b int )
+	result = a+b
+	return 
+} 
+
+func multiply(x float32 , y float32) (res float32){
+	res = x*y;
+    return
+}
